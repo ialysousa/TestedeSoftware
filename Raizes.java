d8089eb90f4bb2fac40cee1cc2db42bb38f1a4dc
lucasEngdComp <lucas.vieira@academico.ifpb.edu.br>
2022-11-17 17:50:55 -0300

    update

--- Raizes.java
+++ Raizes.java
@@ -1,3 +1,10 @@
+import java.lang.reflect.Constructor;
+
 public class Raizes {
-  
+    private double raiz1, raiz2;
+
+    Raizes (double raiz1, double raiz2) {
+        this.raiz1 = raiz1;
+        this.raiz2 = raiz2;
+    }
 }
--- Sistema.java
+++ Sistema.java
@@ -1,14 +1,91 @@
+import javax.swing.RootPaneContainer;
+
 public class Sistema implements FuncionalidadesIF {
 
   private float resultadoAtual = 0;
 
   public Usuario cadastraUsuario(String nome, String email, String senha){return null;}
 
-  public Raizes calculaFuncaoSegundoGrau(double a, double b, double c){return null;}
+  public Raizes calculaFuncaoSegundoGrau(double a, double b, double c) {
+        double dis = b * b - 4.0 * a * c;  
+        if(a < 0 || dis < 0.0) {
+          return 0;
+        }
+          
+        if (dis> 0.0){  
+            double x1 = (-b + Math.pow(dis, 0.5)) / (2.0 * a);  
+            double x2 = (-b - Math.pow(dis, 0.5)) / (2.0 * a);  
+            System.out.println("As raizes são " + x1 + " and " + x2);  
+        }else if (dis == 0.0){  
+            double x1 = -b / (2.0 * a);  
+            System.out.println("Raiz eh " + x1);
 
-  public boolean ehTriangulo(double angulo1, double angulo2, double angulo3){return false;}
+        }   
+        else {
+          System.out.println("As raizes nao sao reais.");
+        }   
 
-  public boolean ehTrianguloRetangulo(double lado1, double lado2, double lado3){return false;}
+
+    } 
+  
+
+  public boolean ehTriangulo(double angulo1, double angulo2, double angulo3){
+      if(angulo1 <= 0 || angulo2 <= 0 || angulo3 <= 0) return false;
+      else if(angulo1 > 180 || angulo2 > 180 || angulo3 > 180) return false;
+      else if(angulo1 + angulo2 >= 180 || angulo1 + angulo3 >= 180 || angulo2 + angulo3 >= 180 || angulo1 + angulo2 + angulo3 > 180) return false;
+      else if (angulo1 + angulo2 + angulo3 == 180 && angulo1 != 0 && angulo2 != 0 && angulo3 != 0)
+          return true;
+      else
+          return false;
+      }
+
+  public boolean ehTrianguloRetangulo(double lado1, double lado2, double lado3){
+      boolean isTrue = false;
+      if((lado1+lado2+lado3 > 180) || lado1 <= 0 || lado2 <= 0 || lado3 <= 0 || (lado1+lado2+lado3 < 0 )){
+          isTrue= false;
+      }
+      else{
+          // retângulo
+          if(lado1+lado2+lado3 == 180){ 
+              //soma dos 3 ângulos tem que ser igual a 180
+              if(lado1 == 90 && lado2+lado3 == 90){
+                  System.out.println("É um triangulo retângulo");
+                  isTrue= true;
+              }
+              else if(lado2 == 90 && lado1+lado3 == 90) {
+                  System.out.println("É um triangulo retângulo");
+                  isTrue= true;
+              }
+              else if(lado3 == 90 && lado1+lado2 == 90){
+                  System.out.println("É um triangulo retângulo");
+                  isTrue= true;
+              }
+          }
+          // acutângulo
+          else if(lado1 < 90 && lado2 < 90 && lado3 < 90){
+              System.out.println("É um triangulo acutângulo");    
+              isTrue= true;
+          }
+
+          //obtusângulo
+          else if(lado1+lado2+lado3 <= 180){
+              if(lado1 > 90 && lado2 + lado3 < 90){
+                  System.out.println("É um triangulo obtusângulo");    
+                  isTrue= true;
+              }
+              else if(lado2 > 90 && lado1 + lado3 < 90){
+                  System.out.println("É um triangulo obtusângulo"); 
+                  isTrue= true;
+              }
+              else if(lado3 > 90 && lado2 + lado1 < 90){
+                  System.out.println("É um triangulo obtusângulo");     
+                  isTrue= true;
+              }
+          }
+          
+      }
+      return isTrue;
+  }
 
   public boolean ehRetangulo(double angulo1, double angulo2, double angulo3, double angulo4){
     if ((angulo1 == angulo2 && angulo1 == angulo3 && angulo1 == angulo4) && (angulo1 != 0)) {
@@ -22,7 +99,35 @@ public class Sistema implements FuncionalidadesIF {
     return false;
   }
 
-  public String classificaTriangulo(double angulo1, double angulo2, double angulo3) {return null;}
+  public String classificaTriangulo(double angulo1, double angulo2, double angulo3) {
+      // triangulo retângulo
+      double hipotenusa = 0;
+      if(angulo1 + angulo2 + angulo3 < 0 || angulo1 + angulo2 + angulo3  > 180 || angulo1 < 0 || angulo2 < 0 || angulo3 < 0){
+          return "Não é um retângulo";
+      }else{
+          if(angulo1 > angulo2 && angulo1 > angulo3){
+              hipotenusa = angulo1;
+              if(hipotenusa*hipotenusa ==  (angulo2*angulo2+angulo3*angulo3)){
+                  return "é um triângulo retângulo";
+              }
+          }
+          if( angulo2 > angulo1 && angulo2 > angulo3){
+              hipotenusa = angulo2;
+              if(hipotenusa *hipotenusa ==  (angulo1*angulo1 + angulo3*angulo3)){
+                  return "é um triângulo retângulo";
+              }
+          }
+          if( angulo3 > angulo1 && angulo3 > angulo2){
+              hipotenusa = angulo3;
+              if(hipotenusa *hipotenusa ==  (angulo1*angulo1 + angulo2*angulo2)){
+                  return "é um triângulo retângulo";
+              }
+          } 
+          return "Não é um triângulo retângulo";
+      }
+  }
+
+  
 
   public double perimetroRetangulo(double lado1, double lado2){
     double perimetro = lado1 + lado2; //+ lado3 + lado4;
@@ -66,10 +171,36 @@ public class Sistema implements FuncionalidadesIF {
     }
   }
 
-  public double areaCirculo(double raio){return 0;}
+  public double areaCirculo(double raio){
+      double pi = 3.14;
+      double area = 0;
 
-  public double perimetroCirculo(double raio){return 0;}
+      if(raio <= 0) return 0;
+      else area=pi*(Math.pow(raio,2));
 
-  public double distanciaEntreDoisPontos(double x1, double y1, double x2, double y2){return 0;}
-  
+      return area;
+  }
+
+  public double perimetroCirculo(double raio){
+      double pi = 3.14;
+      double perimetro;
+      
+      if(raio <= 0) return 0;
+      else perimetro=2*pi*raio;
+      
+      return perimetro;
+  }
+
+
+  public double distanciaEntreDoisPontos(double x1, double y1, double x2, double y2){
+        double ans = 0;
+        double sum = 0;
+        if(x1 <= 0 && x2 <= 0 && y1 <= 0 && y2 <= 0) return 0;
+        else{
+            sum = Math.pow(x1 - x2,2) + Math.pow(y2 - y1,2); 
+            ans = Math.sqrt(sum);
+        }
+        return ans;
+    }
 }
+  
